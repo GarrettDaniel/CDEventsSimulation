5efb3b3a773e9a3f7bcd48707471127498ca397e
Daniel Garrett <36463300+GarrettDaniel@users.noreply.github.com>
2023-03-27 15:31:16 -0600

    change schema to include "run_errors" instead of "errors"

--- PipelineRun.py
+++ PipelineRun.py
@@ -48,11 +48,12 @@ class PipelineRun():
                 
                 possible_errors = ['Invalid input param 123', 'Timeout during execution', 'pipelineRun cancelled by user', 'Unknown error']
                 self.errors = random.choices(possible_errors, weights=(10, 30, 30, 30), k=1)[0]
+                self.entry['run_errors'] = self.errors
                 
             elif self.outcome is 'failure':
                 
                 self.errors = "Unit tests failed"
-                self.entry['errors'] = self.errors
+                self.entry['run_errors'] = self.errors
         
         return
     
